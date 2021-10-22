1091a9d79eac5c4838edddefb431b79a0c97f6ee
Alsalira <liraalsaamalia@gamil.com>
2021-10-22 14:16:14 +0700

    (Video 9. Database Seeder) Membuat halaman authors

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,12 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('12345')
         ]);
 
+        User::create([
+            'name' => 'Dhiya Ulhaq',
+            'email' => 'dhiyaulhaq@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
         Category::create([
             'name' => 'Web Programming',
             'slug' => 'web programming'
@@ -67,7 +73,7 @@ class DatabaseSeeder extends Seeder
             'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, optio, corporis sit itaque, provident praesentium laudantium repudiandae temporibus maxime autem animi. Similique alias cum error totam, corrupti quidem sequi ea doloremque a cupiditate harum voluptas commodi minima sapiente velit incidunt officiis nam.',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quis quam nam molestias aspernatur accusamus aperiam ad, doloribus ea quos laboriosam autem soluta eius vel inventore magnam quibusdam illum facilis similique adipisci ex ipsam pariatur. Voluptas at nam officia error quo eos iure, perspiciatis facere officiis dolorum porro quaerat dolores sunt, cupiditate, itaque maiores consequatur illum dolore laborum voluptates id delectus. Laborum tenetur ipsum numquam eum, obcaecati et alias assumenda quisquam perspiciatis maxime placeat facilis animi recusandae accusantium minus non perferendis dolorum quo neque esse.',
             'category_id' => 2,
-            'user_id' => 1
+            'user_id' => 2
         ]);
     }
 }
