248c8245709270149d93a964a6cf7401421e97d1
Alsalira <liraalsaamalia@gamil.com>
2021-10-22 09:17:51 +0700

    (Video 9. Database Seeder) menambahkan link author

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,12 +5,17 @@
     
 
 @foreach ( $posts as $post)
-<article class="mb-5">
+<article class="mb-5 border-bottom pb-4">
     <h2>
-        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+        <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
     </h2>
+
+    <p>By. Alsa Lira in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">{{ $post->category->name }}</a></p>
+    
     <h5>By: {{ $post->author }}</h5>
     <p>{{ $post->excerpt }}</p>
+
+    <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read More..</a>
 </article>
 @endforeach
 
